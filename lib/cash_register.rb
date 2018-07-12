8cce83ed8bc40f4e0e8d1243c8e2d84ae9d42a75
Jordan Harris <jharris@wework.com>
2018-07-12 20:21:48 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,7 +26,7 @@ class CashRegister
     end
     discount = (@total * @discount/100.0)
     @total = @total - discount 
-    puts "After the discount, the total comes to $#{@total.to_s.chomp("0")}"
+    return "After the discount, the total comes to $#{@total.to_s.chomp("0")}"
   end
   
   
